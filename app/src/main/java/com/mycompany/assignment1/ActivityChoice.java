2b480cc13dbf8f22b3eeb868cd06c5e2fa6678ee
hatchard <hatchard@ualberta.ca>
2015-09-26 14:43:13 -0600

    gameshow to multiplayer button implemented

---
+++ app/src/main/java/com/mycompany/assignment1/ActivityChoice.java
@@ -0,0 +1,44 @@
+package com.mycompany.assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class ActivityChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_choice);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_choice, menu);
+        return true;
+    }
+
+    //http://stackoverflow.com/questions/24610527/how-do-i-get-a-button-to-open-another-activity-in-android-studio 2015/09/26
+    public void goToPlayerSelection(View view) {
+        startActivity(new Intent(ActivityChoice.this, Multiplayer.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
